cd5ae3337e9d30513e906cd82a100a8d8866b337
Alexandra <lexyvyazmich@gmail.com>
2024-05-26 22:13:37 +0700

    added create table funck

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+import os
+import requests
 import pandas as pd
+import openpyxl
 import matplotlib.pyplot as plt
+from config import NASA_API_KEY
+
+url = 'https://api.nasa.gov/DONKI/CMEAnalysis'
+parameters = {
+    'startDate': '',
+    'endDate': '',
+    'mostAccurateOnly': 'true',
+    'speed': '500',
+    'halfAngle': '30',
+    'catalog': 'ALL',
+    'api_key': NASA_API_KEY   
+}
+
+def request_data():
+    response = requests.get(url, params=parameters)
+    try:    
+        data = response.json()
+        df = pd.DataFrame(data)
+        df.to_excel('dataframe.xlsx', index=False)
+        print('всё ок')
+    except:
+        print('чёт не то ')
 
 
-lsts, gender, height = [], [], []
-name = 'name'
-try:
-    with open(f'./files/{name}.csv', 'r') as file:
-        lst = file.readlines()
-        for i, f in enumerate(lst):
-            lsts.append(f.split(','))
-            gender.append(lsts[i][1])
-            height.append(int(lsts[i][3]))
-except:
-    print('файл не открывается')
+#request_data()
+x = []
+table = pd.read_excel('dataframe.xlsx')
+for i in table['time21_5']:
+    x.append(i[5:10])
+y = table['speed']
 
-'''x = ['male', 'female']
-y = [gender.count('Male'), gender.count('Female')]
-plt.bar(x, y, label='gender')
-plt.legend()
-plt.show()'''
-
-x = sorted(height)
-
-plt.figure(figsize=(12, 7))
-plt.plot(x)
+plt.plot(x, y)
 plt.show()
